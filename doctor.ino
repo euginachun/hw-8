e08a45984e6be9fc42d8a39949db1d87a455686a
euginachun <35680934+euginachun@users.noreply.github.com>
2018-03-14 00:21:20 -0700

    Create doctor.ino

---
+++ doctor.ino
@@ -0,0 +1,35 @@
+int sensorValue = 0;
+int sensorPin = A0;
+
+void setup()                    
+{
+ 
+  pinMode(11, OUTPUT);      
+  pinMode(12, OUTPUT);
+  pinMode(13, OUTPUT);
+}
+
+void loop()                      
+{ sensorValue = analogRead(sensorPin);
+
+  if (sensorValue == 1023)
+    digitalWrite(11, LOW);
+    digitalWrite(12, LOW);
+    digitalWrite(13, LOW);
+    sensorValue = analogRead(sensorPin)
+} else if (sensorValue <= 255) {
+    digitalWrite(11, HIGH);
+    delay(1000);
+    digitalWrite(11, LOW);
+    delay(1000);
+    sensorValue = analogRead(sensorPin)
+}else if (sensorValue <= 511) {
+    digitalWrite(12, HIGH);
+    sensorValue = analogRead(sensorPin)
+}else if (sensorValue <= 767) {
+    digitalWrite(13, HIGH);
+    delay(1000);
+    digitalWrite(13, LOW);
+    delay(1000);
+    sensorValue = analogRead(sensorPin)
+}
